350b0494c644d8a8e2dce8a67ef34b37d282c818
Mwambu Kaumba <mwambukaumba63@gmail.com>
2024-07-21 02:08:04 +0200

    UI improvements

---
+++ uniconnect/src/app/pages/profile/page.tsx
@@ -0,0 +1,9 @@
+import NavigationBar from "@/lib/components/NavigationBar";
+
+export default function Profile() {
+    return(
+        <div className="flex p-3 bg-gray-200 shadow-xl min-h-screen">
+            <NavigationBar/>
+        </div>
+    );
+}
--- uniconnect/src/lib/components/MakeApost.tsx
+++ uniconnect/src/lib/components/MakeApost.tsx
@@ -11,19 +11,27 @@ export default function MakeApost() {
             <input className="bg-gray-100 w-full p-3 rounded-lg" placeholder="Write something..."></input>
             <div className="flex justify-end items-center gap-5 w-full p-5">
                 <div className="">
-                   <BiVideo/>
+                    <div className="flex justify-center items-center">
+                        <BiVideo/>
+                    </div>
                    <h1 className="text-xs text-gray-400 text-center">Video</h1>
                 </div>
                 <div>
-                    <FcGallery/>
+                    <div className="flex justify-center items-center">
+                        <FcGallery/>
+                    </div>
                     <h1 className="text-xs text-gray-400">Photos</h1>
                 </div>
                 <div>
-                    <FaLocationPin className="text-green-300"/>
+                    <div className="flex justify-center items-center">
+                        <FaLocationPin className="text-green-300"/>
+                    </div>
                     <h1 className="text-xs text-gray-400">Location</h1>
                 </div>
                 <div>
-                    <FaSmile className="text-yellow-400"/>
+                    <div className="flex justify-center items-center">
+                        <FaSmile className="text-yellow-400"/>
+                    </div>
                     <h1 className="text-xs text-gray-400">Feeling</h1>
                 </div>
                 <button className="bg-blue-500 text-white p-1 rounded-lg">Post</button>
